ed07bef0077ca832c8ac2fca71cd8660b13fd24f
xara-111 <believeinyourself4922@gmail.com>
2024-02-17 20:27:57 +0530

    c++

---
+++ rough.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h;
+    float area;
+    cout<<"Enter base and height: ";
+    cin>>b>>h;
+    area=(float)(b*h)/2;
+    cout<<area;
+    return 0;
+}
--- session5.cpp
+++ session5.cpp
@@ -1,4 +1,6 @@
 /*
+//HELLO WORLD
+
 #include<iostream>
 using namespace std;
 int main()
@@ -12,6 +14,9 @@ int main()
 }
 */
 
+/*
+//TAKING STRING USER INPUT
+
 #include<iostream>
  using namespace std;
  int main()
@@ -19,7 +24,80 @@ int main()
     string name;
 
     cout<<"May i know your name: ";
-    getline(cin, name);
+    //cin>>name;            // for a single word input
+    getline(cin, name);    //for multiple word input
     cout<<"Welcome, "<<name;
     return 0;
  }
+ */
+
+/*
+//AREA OF TRIANGLE
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float base;
+    float height;
+    float area;
+    cout<<"Enter base: ";
+    cin>>base;
+    cout<<"Enter height: ";
+    cin>>height;
+
+    area=(base*height)/2;
+    cout<<"Area is "<<area;
+    return 0;
+    
+}
+*/
+
+/*
+//AREA OF TRIANGLE (BETTER)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,area;
+    cout<<"Enter base and height: ";
+    cin>>b>>h;
+    area=(b*h)/2;
+    cout<<"Area is "<<area;
+    return 0;
+}
+*/
+
+/*
+//AREA OF TRIANLE (TYPE CASTING)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b=7;
+    int h=5;
+    float area;
+    area=(float)(b*h)/2;
+    cout<<area;
+    return 0;
+}
+*/
+
+/*
+//AREA OF TRIANGLE (TYPE CASTING, USER INPUT)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h;
+    float area;
+    cout<<"Enter base and height: ";
+    cin>>b>>h;
+    area=(float)(b*h)/2;
+    cout<<area;
+    return 0;
+}
+*/
