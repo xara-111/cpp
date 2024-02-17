5bd0b26714d00e75519633e934f8bc90c0238f75
xara-111 <believeinyourself4922@gmail.com>
2024-02-17 09:03:47 +0530

    First

---
+++ session5.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter 2 no.s ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition is\n"<<c;
+    return 0;
+}
+*/
+
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+    string name;
+
+    cout<<"May i know your name: ";
+    getline(cin, name);
+    cout<<"Welcome, "<<name;
+    return 0;
+ }
