5bee6ce91ab8b3d4bfb2d09d9562f04f5232bab5
xara-111 <believeinyourself4922@gmail.com>
2024-02-19 23:07:44 +0530

    c++

---
+++ rough.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=5,y;
+    y=~x;
+    cout<<(int)y<<endl;
+    return 0;
+}
--- session5.cpp
+++ session5.cpp
@@ -140,3 +140,166 @@ int main()
 }
 */
 
+/*
+//FINDING ROOTS OF A QUADRATIC EQUATION (ONLY REAL AND DISTINCT)
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+int a,b,c;
+float root1,root2;
+ cout<<"Enter 3 values";
+ cin>>a>>b>>c;
+ root1=(-b+sqrt((b*b)-(4*a*c)))/(2*a);
+ root2=(-b-sqrt((b*b)-(4*a*c)))/(2*a);
+ cout<<root1<<" "<<root2<<endl;
+return 0;
+}
+*/
+
+/*
+//ROOTS OF QUADRATIC EQUATION
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a,b,c,d,r1,r2;
+
+    cout<<"Enter the coefficients of quadratic equation(a,b,c): ";
+    cin>>a>>b>>c;
+
+    d=((b*b)-(4*a*c));
+
+    if(d>0)
+    {
+        r1=(-b+(sqrt(d)))/(2*a);
+        r2=(-b-(sqrt(d)))/(2*a);
+        cout<<"Roots are "<<r1<<" "<<r2;
+    }
+    else if(d==0.0)
+    {
+        r1=(-b)/(2*a);
+        cout<<"Root is "<<r1;
+    }
+    else if(d<0)
+    {
+        r1=(-b)/(2*a);
+        r2=(sqrt(d))/(2*a);       //giving NaN (problem)
+        cout<<"Roots are "<<r1<<" "<<r2;
+    }
+    return 0;
+}
+*/
+
+
+/*
+//AREA OF A CIRCLE
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float area,radius;
+    cout<<"Enter radius: ";
+    cin>>radius;
+
+    //area=22/7*radius*radius;         //radius=7 and area=147(not exact value)
+    //area=22/7.0*radius*radius;       //radius=7 and area=154
+    //area=3.14f*radius*radius;        //radius=7 gives area=153.86
+    //area=(float)22/7*radius*radius;  //radius=7 gives area=154
+    
+    cout<<"Area: "<<area;
+    cout<<"\n";
+    return 0;
+}
+*/
+
+//Distance between 2 points: sqrt(x2-x1)^2+(y2-y1)^2 
+
+/*
+//INCREEMENT AND DECREEMENT
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=5;
+
+    //int j=i++;
+    //int k=++i;
+
+    //cout<<i<<" "<<j ;
+    //cout<<i<<" "<<k ;
+
+    //int j=(2*i++)+(2*i++);  // i=7 and j=22
+
+    return 0;
+}
+*/
+
+
+/*
+//OVERFLOW
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    //char x=127;
+    int x=INT_MAX;
+    ++x;
+    cout<<(int)x;
+}
+*/
+
+/*
+//BITWISE OPERATORS(AND, OR, XOR)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=11,y=7,z;
+    //z=x&y;  // and operator
+    //z=x|y;  //or operator
+    //z=x^y;  //xor operator
+    
+    cout<<z;
+    return 0;
+}
+*/
+
+/*
+//BITWISE (<< , >>)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x=10,y,z;
+    y=x<<1;       //left shift: variable gets multiplied by 2^1(n)
+    z=x>>1;       //right shift: variable gets divided by 2^1(n) 
+
+    cout<<(int)y<<endl;
+    cout<<(int)z<<endl;
+    return 0;
+}
+*/
+
+/*
+//BITWISE (NOT)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=5,y;
+    y=~x;
+    cout<<(int)y<<endl;
+    return 0;
+}
+*/
